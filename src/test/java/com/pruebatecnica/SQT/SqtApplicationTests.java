7f0b416338a3b1d10f99003e9f23aa5bb61cddd5
carlosminatoro <carlos.mina@dreamcodesoft.com>
2024-01-12 10:40:11 -0500

    Primer commit

---
+++ src/test/java/com/pruebatecnica/SQT/SqtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.SQT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
